1a4a45e76c0cd899e5b50492accd6f2f1941c86f
Florian Förster <florian.foerster@uni-leipzig.de>
2022-02-09 14:47:39 +0100

    Removed ResearchArea Model

--- ext_tables.sql
+++ ext_tables.sql
@@ -47,10 +47,6 @@ CREATE TABLE tx_rcgprojectdb_domain_model_sociallink (
 	url varchar(255) NOT NULL DEFAULT ''
 );
 
-CREATE TABLE tx_rcgprojectdb_domain_model_researcharea (
-	title varchar(255) NOT NULL DEFAULT ''
-);
-
 CREATE TABLE tx_rcgprojectdb_domain_model_project (
 	categories int(11) unsigned DEFAULT '0' NOT NULL
 );
