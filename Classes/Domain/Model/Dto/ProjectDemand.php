989ba9bdfd1cece2e82f71f63e82960d54dbb30f
Florian Förster <florian.foerster@uni-leipzig.de>
2022-02-21 14:28:07 +0100

    feat(projects): Create project filter DTO

--- Classes/Domain/Model/Dto/ProjectDemand.php
+++ Classes/Domain/Model/Dto/ProjectDemand.php
@@ -1,6 +1,10 @@
 <?php
 
-declare(strict_types=1);
+namespace ReCentGlobe\Rcgprojectdb\Domain\Model\Dto;
+
+use ReCentGlobe\Rcgprojectdb\Domain\Model\Category;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 /**
  * This file is part of the "ReCentGlobe Database" Extension for TYPO3 CMS.
@@ -11,16 +15,137 @@ declare(strict_types=1);
  * (c) 2022 Florian Förster <florian.foerster@uni-leipzig.de>, ReCentGlobe
  */
 
-namespace ReCentGlobe\Rcgprojectdb\Domain\Model\Dto;
-
-use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
-
 /**
- * ProjectDemand
+ * Demand
  */
 class ProjectDemand extends AbstractEntity
 {
-    // TODO: Build Project DTO for Filter
+
+    /**
+     * @var string The search query
+     */
+    protected string $searchQuery = '';
+
+    /**
+     * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category> projectType category
+     */
+    protected $projectType = null;
+
+    /**
+     * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category> projectRegion category
+     */
+    protected $projectRegion = null;
+
+    /**
+     * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category> projectEra category
+     */
+    protected $projectEra = null;
+
+    /**
+     * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category> projectDiscipline category
+     */
+    protected $projectDiscipline = null;
+
+    /**
+     * @var ObjectStorage<\TYPO3\CMS\Extbase\Domain\Model\Category> researchArea category
+     */
+    protected $researchArea = null;
+
+    /**
+     * @return string
+     */
+    public function getSearchQuery(): string
+    {
+        return $this->searchQuery;
+    }
+
+    /**
+     * @param string $searchQuery
+     */
+    public function setSearchQuery(string $searchQuery): void
+    {
+        $this->searchQuery = $searchQuery;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getProjectType(): ?ObjectStorage
+    {
+        return $this->projectType;
+    }
+
+    /**
+     * @param ObjectStorage $projectType
+     */
+    public function setProjectType(?ObjectStorage $projectType): void
+    {
+        $this->projectType = $projectType;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getProjectRegion(): ?ObjectStorage
+    {
+        return $this->projectRegion;
+    }
+
+    /**
+     * @param ObjectStorage $projectRegion
+     */
+    public function setProjectRegion(?ObjectStorage $projectRegion): void
+    {
+        $this->projectRegion = $projectRegion;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getProjectEra(): ?ObjectStorage
+    {
+        return $this->projectEra;
+    }
+
+    /**
+     * @param ObjectStorage $projectEra
+     */
+    public function setProjectEra(?ObjectStorage $projectEra): void
+    {
+        $this->projectEra = $projectEra;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getProjectDiscipline(): ?ObjectStorage
+    {
+        return $this->projectDiscipline;
+    }
+
+    /**
+     * @param ObjectStorage $projectDiscipline
+     */
+    public function setProjectDiscipline(?ObjectStorage $projectDiscipline): void
+    {
+        $this->projectDiscipline = $projectDiscipline;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getResearchArea(): ?ObjectStorage
+    {
+        return $this->researchArea;
+    }
+
+    /**
+     * @param ObjectStorage $researchArea
+     */
+    public function setResearchArea(?ObjectStorage $researchArea): void
+    {
+        $this->researchArea = $researchArea;
+    }
 
 
 }
